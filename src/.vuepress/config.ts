88fd952b6a8a9a057621a0524534edba326af54b
Mly66 <3127328609@qq.com>
2025-03-14 22:56:19 +0800

    icon demo

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,6 +3,12 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
+  head: [
+    // 设置 favor.ico，.vuepress/public 下
+    [
+        'link', { rel: 'icon', href: 'icon.png' }
+    ]
+],
   base: "/blog/",
 
   lang: "zh-CN",
